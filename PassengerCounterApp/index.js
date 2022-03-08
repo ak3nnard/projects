947a35b1fbf26911f881b88f21611ea5b583bf78
ak3nnard <97188048+ak3nnard@users.noreply.github.com>
2022-03-08 14:28:06 -0500

    Add files via upload

---
+++ PassengerCounterApp/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+let countEl = document.getElementById('count-el')
+let count = 0
+let saveEl = document.getElementById('save-el')
+
+function increment(){
+	count += 1
+	countEl.innerText = count
+};
+
+// function save() logs out the count when its called
+
+function save() {
+	let countStr = count + ' - '
+	saveEl.textContent += countStr
+	countEl.textContent = 0
+	count = 0
+};
+
+
